31aa91f1ac12a9c0515fdade23912a2d2443569a
ryankayne <coope1rk@gmail.com>
2020-01-24 09:25:36 -0800

    table created in SQLite

--- data/migrations/20200124092132_projects.js
+++ data/migrations/20200124092132_projects.js
@@ -3,11 +3,11 @@ exports.up = function(knex) {
     return knex.schema
     .createTable('project', tbl => {
     tbl.increments();
-    tbl.text('project_id')
+    tbl.text('project_id');
     tbl.text('name')
-    .notNullable()
-    tbl.text('project_description')
-    tbl.boolean('completed')
+    .notNullable();
+    tbl.text('project_description');
+    tbl.boolean('completed');
     
      
  })
@@ -16,24 +16,24 @@ exports.up = function(knex) {
     tbl.integer('task_id')
     .unsigned()
     .references('project_id')
-    .inTable('project')
+    .inTable('project');
     tbl.text('task_description')
-    .notNullable()
-    tbl.text('task_notes')
+    .notNullable();
+    tbl.text('task_notes');
  })
  
  .createTable('resources', tbl => {
     tbl.increments();
     tbl.integer('resource_id')
-                .unsigned()
-                .notNullable()
-                .references('project_id')
-                .inTable('project')
-                // .onDelete('RESTRICT')
-                // .onUpdate('CASCADE'); 
-    tbl.text('resource_name')
+    .unsigned()
     .notNullable()
-    tbl.text('resource_description')
+    .references('project_id')
+    .inTable('project');
+    // .onDelete('RESTRICT')
+    // .onUpdate('CASCADE'); 
+    tbl.text('resource_name')
+    .notNullable();
+    tbl.text('resource_description');
  })
 };
 
@@ -41,5 +41,5 @@ exports.down = function(knex) {
     return knex.schema 
     .dropTableIfExists('resources')
     .dropTableIfExists('task')
-    .dropTableIfExists('project')
+    .dropTableIfExists('project');
 };
